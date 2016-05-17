68a3c7a3f1e496e13c80369a1b2795bcb2635b4f
Adam King <adam.king0126@gmail.com>
2016-05-17 15:49:14 -0500

    update dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,4 +1,24 @@
 var dictionary = {
-  "hey": "yo, ho, ho",
-  "punishment": "walk the plank"
+  "hooray": "yo, ho, ho",
+  "punishment": "walk the plank",
+  "drunk": "three sheets to the wind",
+  "you suck": "son of a biscuit eater",
+  "punk": "son of a biscuit eater",
+  "jerk": "son of a biscuit eater",
+  "look!": "that she blows!",
+  "doomed": "shark bait",
+  "old guy": "salty dog",
+  "fool": "scalliwag",
+  "treasure": "booty",
+  "whoa": "blimey",
+  "flag": "jolly roger",
+  "sword": "cutlass",
+  "ship": "man-o-war",
+  "car": "man-o-war",
+  "hurry up": "heave ho",
+  "get ready": "batten down the hatches",
+  "ahoy": "hello",
+  "wow": "blimey",
+  
+
 }
